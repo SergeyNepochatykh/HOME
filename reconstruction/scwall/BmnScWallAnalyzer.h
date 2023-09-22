ab9453e84a1f00e05bad88fb0f5133daafbb461b
SergeyNepochatykh <sergio96903@gmail.com>
2023-09-22 13:23:56 +0300

    Changes to Reconstruction of Scint. Wall

--- reconstruction/scwall/BmnScWallAnalyzer.h
+++ reconstruction/scwall/BmnScWallAnalyzer.h
@@ -7,12 +7,34 @@
 #ifndef BMNSCWALLANALYZER_H
 #define BMNSCWALLANALYZER_H
 
+#define DO_SORT 1 
+
+#include "FairLogger.h"
+#include <TStopwatch.h>
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
 #include <FairTask.h>
 #include <TClonesArray.h>
 
 #include "BmnScWallReconstructor.h"
 #include "BmnScWallDigi.h"
 #include "DstEventHeader.h"
+#include "BmnDchTrack.h"
+
+#pragma GCC system_header
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/foreach.hpp>
+
+namespace pt = boost::property_tree;
+
+using MAP1 = map <UInt_t, vector <UInt_t>>;
+
+using MAP2 = map <UInt_t, vector <Double_t>>;
 
 class BmnScWallAnalyzer : public FairTask
 {
@@ -23,10 +45,12 @@ class BmnScWallAnalyzer : public FairTask
 
     virtual InitStatus Init();
     
+    virtual InitStatus ReInit();
+    
     virtual void Exec(Option_t* opt);
     
     virtual void Finish();
-
+    
   private:
     TClonesArray * fArrayOfScWallDigits; 
   
@@ -44,9 +68,31 @@ class BmnScWallAnalyzer : public FairTask
     
     TString fmapFile;
     
-    std::map <UInt_t, std::vector <Double_t>> fScWallMapper;
+    MAP2 fScWallMapper;
     
-    std::map <UInt_t, std::vector <Double_t>> GetMapper(TString mapFile);
+    MAP2 GetMapper(TString);
+    
+    MAP1 fScWallNeighbour;
+    
+    MAP1 GetNeighbour();
+    
+    void Do_Reconstruction();
+    
+    vector <vector <Double_t>> fRESULT; 
+    
+    void Fill_Dst_Event_Header(TStopwatch& SW, vector <vector <Double_t>> RESULT={}, TString OPTION="");
+    
+    vector <UInt_t> Is_UD_and_Match_Status(Double_t&, Double_t&, vector <Double_t>&, vector <Double_t>&, vector <UInt_t>&, MAP1&, MAP2&);
+    
+    UInt_t Is_Small_Next_Neighbour_of_Small_Cell(Double_t&, Double_t&, UInt_t&, MAP1&, MAP2&);
+    
+    void Put_Info_From_DCH(vector <Double_t>&, vector <Double_t>&);
+    
+    void Put_Info_From_ScWall(vector <Double_t>&, vector <Double_t>&, vector <Double_t>&, vector <UInt_t>&);
+    
+    Bool_t Is_in_Box(Double_t, Double_t, Double_t, Double_t, Double_t, Double_t);
+    
+    void Do_Calibration(Double_t&); 
 
     ClassDef(BmnScWallAnalyzer, 1);
 };
